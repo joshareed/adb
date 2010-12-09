eb696602555483643cb757bc7e263ddeab62cc3a
Josh Reed <jareed@andrill.org>
2010-12-09 09:00:40 -0600

    Set default values for Project fields

--- grails-app/domain/org/andrill/adb/Project.groovy
+++ grails-app/domain/org/andrill/adb/Project.groovy
@@ -3,11 +3,12 @@ package org.andrill.adb
 class Project {
 
 	String name
-	String description
-	String url
+	String description = ""
+	String url = ""
 
     static constraints = {
-		code blank: false, unique: true
-		name blank: false
+		name 			blank: false
+		description		blank: true
+		url				blank: true
     }
 }
